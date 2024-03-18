cbcce6b54fb317b64234b75f874aaead7ebb87bb
Raghav <raghavawasthi2005@gmail.com>
2024-03-18 01:58:24 +0000

    created Column class & met requirement 1

---
+++ src/main/java/ucl/ac/uk/dataHolders/Column.java
@@ -0,0 +1,30 @@
+package ucl.ac.uk.dataHolders;
+
+import java.util.ArrayList;
+
+public class Column {
+
+    private String name;
+    private ArrayList<String> rows = new ArrayList<>();
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSize() {
+        return rows.size();
+    }
+
+    public String getRowValue(int rowNumber) { // indexing begins at 1
+        return rows.get(rowNumber - 1);
+    }
+
+    public void setRowValue(int rowNumber, String newValue) { // indexing begins at 1
+        rows.set(rowNumber - 1, newValue);
+    }
+
+    public void addRowValue(String newValue) {
+        rows.add(newValue);
+    }
+
+}
